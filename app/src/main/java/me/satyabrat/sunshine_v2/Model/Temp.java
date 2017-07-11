53f5882853f512705f3c42d832a69f8d9b784383
thelazyindian <satyabrat.me@gmail.com>
2017-07-11 07:46:43 +0530

    No need of this

--- app/src/main/java/me/satyabrat/sunshine_v2/Model/Temp.java
+++ app/src/main/java/me/satyabrat/sunshine_v2/Model/Temp.java
@@ -1,27 +1,20 @@
 package me.satyabrat.sunshine_v2.Model;
 
-import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Temp {
 
     @SerializedName("day")
-    @Expose
     public double day;
     @SerializedName("min")
-    @Expose
     public double min;
     @SerializedName("max")
-    @Expose
     public double max;
     @SerializedName("night")
-    @Expose
     public double night;
     @SerializedName("eve")
-    @Expose
     public double eve;
     @SerializedName("morn")
-    @Expose
     public double morn;
 
 }
--- app/src/main/java/me/satyabrat/sunshine_v2/Model/WeatherJson.java
+++ app/src/main/java/me/satyabrat/sunshine_v2/Model/WeatherJson.java
@@ -1,24 +1,18 @@
 package me.satyabrat.sunshine_v2.Model;
 
-import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class WeatherJson {
 
     @SerializedName("city")
-    @Expose
     public City city;
     @SerializedName("cod")
-    @Expose
     public String cod;
     @SerializedName("message")
-    @Expose
     public double message;
     @SerializedName("cnt")
-    @Expose
     public int cnt;
     @SerializedName("list")
-    @Expose
     public java.util.List<me.satyabrat.sunshine_v2.Model.List> list = null;
 
 }
